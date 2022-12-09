b842de9c46dbb177419af47b07d1b6d846e47c6b
pinrongc <pinrongc156@gmail.com>
2022-12-09 14:00:15 -0500

    hl

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,15 +1,149 @@
 const menu = [ 
  { 
-    name: "first",
-    image:"" ,
+    name: "Pork Fried Rice",
+    image:"https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FPhoto%2FSeries%2F2020-04-snapshot-cooking-5-recipes-to-make-with-2-cups-of-rice%2Fthekitchn-pork-fried-rice-beauty" ,
+    type: ["Lunch", "Dinner"]
 },
 
 {
-    name: "second",
-    image: "",
+    name: "Sushi",
+    image: "https://howdaily.com/wp-content/uploads/2017/05/zushi-sushi.jpg",
+    type: ["Lunch", "Dinner"]
 
 },
 
+{
+    name: "Chicken Alfredo Pizza",
+    image: "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FPhoto%2FRecipe%20Ramp%20Up%2F2021-07-Chicken-Alfredo-Pizza%2FChicken-Alfredo-Pizza-KitchnKitchn2970-1_01" ,
+    type: ["Lunch", "Dinner"]
+},
+
+{
+    name: "Pho",
+    image: "https://pyxis.nymag.com/v1/imgs/a10/6f9/b38ee0282c2ac12bad2d5abac2d4034ec0-bony-pho-an-choi.2x.rhorizontal.w710.jpg",
+    type: "Breakfast"
+
+},
+
+{
+    name: "Pinwheel-Sandwiches",
+    image: "https://amandascookin.com/wp-content/uploads/2021/07/Pinwheel-Sandwiches-RC-SQ.jpg",
+    type: "Lunch"
+
+},
+
+{
+    name: "Syrniki",
+    image:"https://izzycooking.com/wp-content/uploads/2022/05/Syrniki-Russian-Cheese-Pancakes-thumbnail.jpg",
+    type: "Breakfast"
+
+},
+/* 
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+},
+
+{
+    name: 
+    image:
+    type: 
+
+}, */
 
 ]; 
 
