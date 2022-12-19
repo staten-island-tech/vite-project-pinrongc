fb16b50a21c3ca165413805032050ccbb04f9b10
pinrongc <pinrongc156@gmail.com>
2022-12-18 22:19:27 -0500

    hi

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -60,11 +60,17 @@ DOMselectors.dinner.addEventListener("click", function() {
 
 })
 
+DOMselectors.theme.addEventListener("click", function() {
+ if(document.body.classList.contains("cool")){
+ document.body.classList.add("warm");
+ document.body.classList.remove("cool");
+ }
+ else{
+  document.body.classList.add("cool");
+  document.body.classList.remove("warm");
 
-
-
-
-
+}
+});
 
 
 menu.forEach (cards);
